624b92f40e6ebe196be8fa6a01bd6f2ea4f2a482
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-08-07 17:02:45 -0400

    Initial Commit

---
+++ LawerUp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LawerUp
+//
+//  Created by Arsalan Vakili on 2015-08-07.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
